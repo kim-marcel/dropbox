d8c5b2eca6c9a6d13881b593d95b7f87ff9fb2d7
kim-marcel <kim.feichtinger@gmx.de>
2018-04-03 16:47:00 +0100

    bugfix: crash when no file selected
    - fixed bug where it would crash when no file was selected on upload

--- uploadhandler.py
+++ uploadhandler.py
@@ -1,15 +1,17 @@
 from google.appengine.ext import blobstore
 from google.appengine.ext.webapp import blobstore_handlers
 import utilities
+import logging
 
 
 class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
     def post(self):
         # Get the data (file) from the request
-        upload = self.get_uploads()[0]
-        # Get file name from the info of the file
-        filename = blobstore.BlobInfo(upload.key()).filename
+        if len(self.get_uploads()) > 0:
+            upload = self.get_uploads()[0]
+            # Get file name from the info of the file
+            filename = blobstore.BlobInfo(upload.key()).filename
 
-        utilities.add_file(upload, filename)
+            utilities.add_file(upload, filename)
 
         self.redirect('/')
